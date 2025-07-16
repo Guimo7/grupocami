9aa67fd848cefb133a54109bced7c3ee611d14c3
DonDyprax <juan.acosta7a@gmail.com>
2025-07-16 16:45:44 -0600

    Change vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
-	base: "https://guimo7.github.io/grupocami",
+	base: "https://guimovfx.com/grupocami",
 });
